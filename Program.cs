e8a351a72b12fcb2ea71c95a04bbd18eb65e39cd
Dhanush H V <dhanushhv75@gmail.com>
2026-04-30 08:56:41 +0000

    Codingbat: Arrays::fizzArray

--- Program.cs
+++ Program.cs
@@ -225,4 +225,14 @@ public static class Program
 
         return count > 0;
     }
+
+    public static int[] FizzArray(int n)
+    {
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = i;
+        
+        return arr;
+    }
 }
