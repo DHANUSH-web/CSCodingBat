8a66782499d58c1dae5771289da6ff259fb8f089
Dhanush H V <dhanushhv75@gmail.com>
2026-05-28 13:36:00 +0000

    Codingbat: Arrays::matchUp

--- Program.cs
+++ Program.cs
@@ -293,4 +293,15 @@ public static class Program
 
         return is2 != is4;
     }
+
+    public static int MatchUp(int[] nums1, int[] nums2)
+    {
+        int count = 0;
+
+        for (int i = 0; i < nums1.Length; i++)
+            if (nums1[i] != nums2[i] && Math.Abs(nums1[i] - nums2[i]) <= 2)
+                count++;
+
+        return count;
+    }
 }
