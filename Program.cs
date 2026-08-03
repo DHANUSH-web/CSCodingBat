730c8e0fb4962b1f81f933cbba5fc8048d0466d6
Dhanush H V <dhanushhv75@gmail.com>
2026-08-03 07:49:25 +0000

    CodingBat: Arrays::modThree

--- Program.cs
+++ Program.cs
@@ -328,4 +328,23 @@ public static class Program
 
         return false;
     }
+
+    public static bool ModThree(int[] nums)
+    {
+        for (int i = 0; i < nums.Length-2; i++)
+        {
+            if ((
+                nums[i]     % 2 == 0 &&
+                nums[i+1]   % 2 == 0 &&
+                nums[i+2]   % 2 == 0
+            ) || (
+                nums[i]     % 2 == 1 &&
+                nums[i+1]   % 2 == 1 &&
+                nums[i+2]   % 2 == 1
+            ))
+                return true;
+        }
+
+        return false;
+    }
 }
